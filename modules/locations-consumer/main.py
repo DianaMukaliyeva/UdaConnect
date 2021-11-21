40cb7f654e872c3017c5c6b0ad298a9f56a4ce14
Diana Mukaliyeva <diana.mukaliyeva@smartly.io>
2021-11-21 02:28:10 +0200

    Add consumer and fix kafka config

---
+++ modules/locations-consumer/main.py
@@ -0,0 +1,36 @@
+import os
+import psycopg2
+from kafka import KafkaConsumer
+from json import loads
+
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+TOPIC_NAME = 'locations'
+KAFKA_SERVER = 'kafka-service:9092'
+
+def db_connect():
+  return psycopg2.connect(dbname=DB_NAME,
+            user=DB_USERNAME,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+          )
+
+def create_location(location):
+  db = db_connect()
+  cursor = db.cursor()
+  cursor.execute('INSERT INTO location (person_id, coordinate) VALUES (%s, ST_POINT(%s, %s))',
+                  (location['person_id'], location['latitude'], location['longitude'])
+                )
+  db.commit()
+  cursor.close()
+  db.close()
+
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=KAFKA_SERVER)
+
+for location in consumer:
+  create_location(loads(location.value.decode("utf-8")))
